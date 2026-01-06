87f05b6e008f44f8754989729475a54ec96518b1
elorenzi23 <elorenzi@elis-air.mynetworksettings.com>
2026-01-06 02:03:09 -0500

    fix tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,8 @@
 import pytest
-from fastapi.testclient import TestClient
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from app.database import Base, get_db
+from app.database import Base
 
 # Use an SQLite database for testing
 TEST_DATABASE_URL = "sqlite:///./test.db"
@@ -23,17 +22,3 @@ def db():
     yield session
     session.close()
     Base.metadata.drop_all(bind=engine)  # Cleanup after tests
-
-
-@pytest.fixture(scope="module")
-def client():
-    def override_get_db():
-        db = TestingSessionLocal()
-        try:
-            yield db
-        finally:
-            db.close()
-
-    app.dependency_overrides[get_db] = override_get_db
-    with TestClient(app) as c:
-        yield c
